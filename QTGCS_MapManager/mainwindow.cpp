f936304fccd04b1f239afa434fe21989d7d726f3
Tairan Liu <liutairan2012@gmail.com>
2018-06-04 09:47:31 -0500

    Work on UI

--- QTGCS_MapManager/mainwindow.cpp
+++ QTGCS_MapManager/mainwindow.cpp
@@ -7,9 +7,92 @@ MainWindow::MainWindow(QWidget *parent) :
 {
     ui->setupUi(this);
     removeToolBar(ui->mainToolBar);
+    createStatusBar();
+    currentWorkingPath = QCoreApplication::applicationDirPath();
+    cfgFilePath = currentWorkingPath + "/config.json";
+
+    QByteArray val;
+    val = readJsonFile(cfgFilePath);
+    QJsonDocument doc;
+    doc = QJsonDocument::fromJson(val);
+    QJsonObject obj = doc.object();
+    resourcePath = obj["Resource"].toString();
+
+    InitToolBox();
+    InitMap();
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+QByteArray MainWindow::readJsonFile(const QString &filename)
+{
+    QFile f(filename);
+    if (!f.open(QFile::ReadOnly | QFile::Text)) {
+        f.close();
+        return QString().toUtf8();
+    } else {
+        QTextStream in(&f);
+        QByteArray retValue = in.readAll().toUtf8();
+        f.close();
+        return retValue;
+    }
+}
+
+void MainWindow::InitToolBox()
+{
+    ui->plusButton->move(640, 10);
+    ui->minusButton->move(640, 50);
+    ui->downloadButton->move(640, 90);
+    ui->returnButton->move(640, 130);
+}
+
+void MainWindow::InitMap()
+{
+    ui->mapLabel->setGeometry(0,0,640,640);
+    QString blackPath = resourcePath + "/res/black.jpg";
+    QImageReader r(blackPath);
+    QImage i = r.read();
+    if (!i.isNull())
+    {
+        imageHandle = QPixmap::fromImage(i);
+    }
+    //QPainter painter(imageHandle);
+
+    //draw(&painter);
+    ui->mapLabel->setPixmap(imageHandle);
+    this->repaint();
+}
+
+void MainWindow::createStatusBar()
+{
+    statusBar()->showMessage(tr("Ready"));
+}
+
+//void MainWindow::resizeEvent(QResizeEvent *event)
+//{
+//    QSize size = this->size();
+//    statusBar()->showMessage("Size"+QString::number(size.width(), 10) + QString::number(size.height(),10));
+//}
+
+void MainWindow::on_plusButton_clicked()
+{
+
+}
+
+void MainWindow::on_minusButton_clicked()
+{
+
+}
+
+void MainWindow::on_downloadButton_clicked()
+{
+
+}
+
+void MainWindow::on_returnButton_clicked()
+{
+
 }
--- QTGCS_MapManager/mainwindow.h
+++ QTGCS_MapManager/mainwindow.h
@@ -2,6 +2,12 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QDebug>
+#include <QPixmap>
+#include <QImageReader>
+#include <QImage>
+#include <QFile>
+#include <QJsonDocument>
 
 namespace Ui {
 class MainWindow;
@@ -14,6 +20,25 @@ class MainWindow : public QMainWindow
 public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
+    void InitToolBox();
+    void InitMap();
+    QPixmap imageHandle;
+    void createStatusBar();
+    QString currentWorkingPath;
+    QString cfgFilePath;
+    QString resourcePath;
+    QString xbeeAddrPath;
+    QByteArray readJsonFile(const QString &filename);
+protected:
+    //void resizeEvent(QResizeEvent *event) override;
+private slots:
+    void on_plusButton_clicked();
+
+    void on_minusButton_clicked();
+
+    void on_downloadButton_clicked();
+
+    void on_returnButton_clicked();
 
 private:
     Ui::MainWindow *ui;
